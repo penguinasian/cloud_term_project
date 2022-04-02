fbfc57524a2817fef4f9a2da16d0cdc402658928
penguinasian <zhangqiangtianchen@gmail.com>
2022-04-02 15:47:00 -0700

    add router for landing page

--- server.js
+++ server.js
@@ -4,40 +4,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Below are the codes for when not using express module
-// const server = http.createServer((req, res) => {
-//     console.log(req.url, req.method);
-//     // set header content type
-//     res.setHeader('Content-Type', 'text/html');
+const { DynamoDBClient, BatchGetItemCommand, BatchWriteItemCommand } = require("@aws-sdk/client-dynamodb");
 
-//     let path = './views/';
-//     switch(req.url) {
-//         case '/':
-//             path += 'index.html';
-//             break;
-//         case '/login':
-//             path += 'login.thml'
-//             break;
-//         default:
-//             path += '404.html';
-//             break;
-//     }
-    
-//     // send an html file
-//     fs.readFile(path, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//             res.end()
-//         } else {
-//             res.write(data);
-//             res.end();
-//         }
-//     })
-// });
-
-// server.listen(3000, 'localhost', () => {
-//     console.log('listening for requests on port number 3000');
-// });
+(async () => {
+    const client = new DynamoDBClient({ region: "us-west-2" });
+    const commandRead = new BatchGetItemCommand({RequestItems: {users: {Keys:[{email: {"S": 'zhangqiangtianchen@gmail.com' }}]}}});
+    const response = await client.send(commandRead);
+    const commandWrite = new BatchWriteItemCommand({RequestItems: {users: [{PutRequest: {Item: {email: {"S": "andrew.paugh@gmail.com"}, password:{ "S": "12345"}}}}]}});
+    await client.send(commandWrite);
+})();
 
 app.listen(3000);
 
@@ -46,16 +21,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routing to sign up page
 app.get('/', (req, res) => {
-    res.sendFile('./views/login.html', {root: __dirname});
+    res.sendFile('./views/login.html', { root: __dirname });
 })
 
 // Routing to login page
 app.get('/signup', (req, res) => {
-    res.sendFile('./views/signup.html', {root: __dirname});
+    res.sendFile('./views/signup.html', { root: __dirname });
+})
+
+//Routing to landing page
+app.post('/landing', (req, res) => {
+
+    res.sendFile('./views/landing_page.html', {root: __dirname});
 })
 
 // 404 page, the use function is going to fire for every request come in, but only if the request only reaches
 // to this line of code.
 app.use((req, res) => {
-    res.sendFile('./views/404.html', {root: __dirname});
+    res.sendFile('./views/404.html', { root: __dirname });
 })
