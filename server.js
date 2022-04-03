10c2a8fc9b1cba57e401eb3205d13ccd18373ba7
penguinasian <zhangqiangtianchen@gmail.com>
2022-04-02 23:58:20 -0700

    add js code to dynamically render landing page and verify user login and signup

--- server.js
+++ server.js
@@ -4,19 +4,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// require dynamodb library
 const { DynamoDBClient, BatchGetItemCommand, BatchWriteItemCommand } = require("@aws-sdk/client-dynamodb");
 
-(async () => {
-    const client = new DynamoDBClient({ region: "us-west-2" });
-    const commandRead = new BatchGetItemCommand({ RequestItems: { users: { Keys: [{ email: { "S": 'andrew.paugh@gmail.com' } }] } } });
-    // email = commandRead.input.RequestItems.users.Keys[0].email.S;
-    // console.log(email);
-    // console.log(email == "zhangqiangtianchen@gmail.com");
-    const response = await client.send(commandRead);
-    console.log(response);
-    const commandWrite = new BatchWriteItemCommand({ RequestItems: { users: [{ PutRequest: { Item: { email: { "S": "andrew.paugh@gmail.com" }, password: { "S": "12345" } } } }] } });
-    await client.send(commandWrite);
-})();
+// require view engine for achiving dynamically modify the static html page - our case, specifically for the landing page
+app.set('view engine', 'ejs');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,37 +16,62 @@ app.use(bodyParser.json());
 
 app.listen(3000);
 
+// setting up variables to manipulate the front end with ejs template
+// Error message, such as "User already exists" "invalid password" will show up depending on the input
+let userNonExistMessage = null;
+let invalidPasswordMessage = null;
+let userExistsMessage = null;
+
 // For linking the css to html files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routing to sign up page
 app.get('/', (req, res) => {
-    res.sendFile('./views/login.html', { root: __dirname });
+    userNonExistMessage = null;
+    invalidPasswordMessage = null;
+    res.render('login', {userNonExistMessage, invalidPasswordMessage});
 })
 
 // Routing to login page
 app.get('/signup', (req, res) => {
-    res.sendFile('./views/signup.html', { root: __dirname });
+    userExistsMessage = null;
+    res.render('signup', {userExistsMessage});
 })
 
 //Routing to landing page when logged in
 app.post('/landing', async (req, res) => {
     let password = req.body.password;
     let email = req.body.email;
-
+    
+    
     const client = new DynamoDBClient({ region: "us-west-2" });
+    // Read data by primary key - email
     const commandRead = new BatchGetItemCommand({ RequestItems: { users: { Keys: [{ email: { "S": email } }] } } });
 
     const responseRead = await client.send(commandRead);
-
+    
     if (responseRead.Responses.users.length == 0) {
+        // set the error message to non null and render it on login page
+        userNonExistMessage = "User does not exit!";
+        invalidPasswordMessage = null;
+        res.render('login', {userNonExistMessage, invalidPasswordMessage});
         console.log("User does not exist!");
+        
     } else {
         if (responseRead.Responses.users[0].password.S != password) {
+            invalidPasswordMessage = "Invalid Password!";
+            userNonExistMessage = null;
+            res.render('login', {userNonExistMessage, invalidPasswordMessage});
             console.log("Incorrect Password!")
         }
         else  {
-            res.sendFile('./views/landing_page.html', { root: __dirname });
+            let firstName = responseRead.Responses.users[0].firstName.S;
+            let lastName = responseRead.Responses.users[0].lastName.S;
+            let titlelizedFirstName = firstName[0].toUpperCase() + firstName.substring(1);
+            let titlelizedLastName = lastName[0].toUpperCase() + lastName.substring(1);
+            firstName[0].toUpperCase();
+            lastName[0].toUpperCase();
+            res.render('landing_page', {titlelizedFirstName, titlelizedLastName});
         }
     }
 
@@ -65,18 +82,27 @@ app.post('/first', async (req, res) => {
 
     let firstName = req.body.firstName;
     let lastName = req.body.lastName;
+    let titlelizedFirstName = firstName[0].toUpperCase() + firstName.substring(1);
+    let titlelizedLastName = lastName[0].toUpperCase() + lastName.substring(1);
+    
     let password = req.body.password;
     let email = req.body.email;
     const client = new DynamoDBClient({ region: "us-west-2" });
     const commandRead = new BatchGetItemCommand({ RequestItems: { users: { Keys: [{ email: { "S": email } }] } } });
 
     const response = await client.send(commandRead);
+    
+
     if (response.Responses.users.length != 0) {
+        // set the error message to non null and render it on signup page
+        userExistsMessage = "User already exists!";
+        res.render('signup', {userExistsMessage});
         console.log("User already exists!")
     } else {
-        const commandWrite = new BatchWriteItemCommand({ RequestItems: { users: [{ PutRequest: { Item: { email: { "S": email }, password: { "S": password } } } }] } });
+        const commandWrite = new BatchWriteItemCommand({ RequestItems: { users: [{ PutRequest: 
+            { Item: { email: { "S": email }, password: { "S": password }, firstName: {"S": firstName}, lastName: {"S": lastName} } } }] } });
         await client.send(commandWrite);
-        res.sendFile('./views/signup_landing_page.html', { root: __dirname });
+        res.render('signup_landing_page', {titlelizedFirstName, titlelizedLastName});
     }
 
 })
