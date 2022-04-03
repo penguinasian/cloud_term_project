d048e87a8290b8faf6d81bf218407863eecd490f
penguinasian <zhangqiangtianchen@gmail.com>
2022-04-02 20:48:55 -0700

    add back end code to do user aauthentication with DynamoDB.

--- server.js
+++ server.js
@@ -8,11 +8,19 @@ const { DynamoDBClient, BatchGetItemCommand, BatchWriteItemCommand } = require("
 
 (async () => {
     const client = new DynamoDBClient({ region: "us-west-2" });
-    const commandRead = new BatchGetItemCommand({RequestItems: {users: {Keys:[{email: {"S": 'zhangqiangtianchen@gmail.com' }}]}}});
+    const commandRead = new BatchGetItemCommand({ RequestItems: { users: { Keys: [{ email: { "S": 'andrew.paugh@gmail.com' } }] } } });
+    // email = commandRead.input.RequestItems.users.Keys[0].email.S;
+    // console.log(email);
+    // console.log(email == "zhangqiangtianchen@gmail.com");
     const response = await client.send(commandRead);
-    const commandWrite = new BatchWriteItemCommand({RequestItems: {users: [{PutRequest: {Item: {email: {"S": "andrew.paugh@gmail.com"}, password:{ "S": "12345"}}}}]}});
+    console.log(response);
+    const commandWrite = new BatchWriteItemCommand({ RequestItems: { users: [{ PutRequest: { Item: { email: { "S": "andrew.paugh@gmail.com" }, password: { "S": "12345" } } } }] } });
     await client.send(commandWrite);
 })();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(3000);
 
@@ -30,15 +38,47 @@ app.get('/signup', (req, res) => {
 })
 
 //Routing to landing page when logged in
-app.post('/landing', (req, res) => {
+app.post('/landing', async (req, res) => {
+    let password = req.body.password;
+    let email = req.body.email;
 
-    res.sendFile('./views/landing_page.html', {root: __dirname});
+    const client = new DynamoDBClient({ region: "us-west-2" });
+    const commandRead = new BatchGetItemCommand({ RequestItems: { users: { Keys: [{ email: { "S": email } }] } } });
+
+    const responseRead = await client.send(commandRead);
+
+    if (responseRead.Responses.users.length == 0) {
+        console.log("User does not exist!");
+    } else {
+        if (responseRead.Responses.users[0].password.S != password) {
+            console.log("Incorrect Password!")
+        }
+        else  {
+            res.sendFile('./views/landing_page.html', { root: __dirname });
+        }
+    }
+
 })
 
 //Routing to landing page after first time sign up
-app.post('/first', (req, res) => {
+app.post('/first', async (req, res) => {
 
-    res.sendFile('./views/signup_landing_page.html', {root: __dirname});
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let password = req.body.password;
+    let email = req.body.email;
+    const client = new DynamoDBClient({ region: "us-west-2" });
+    const commandRead = new BatchGetItemCommand({ RequestItems: { users: { Keys: [{ email: { "S": email } }] } } });
+
+    const response = await client.send(commandRead);
+    if (response.Responses.users.length != 0) {
+        console.log("User already exists!")
+    } else {
+        const commandWrite = new BatchWriteItemCommand({ RequestItems: { users: [{ PutRequest: { Item: { email: { "S": email }, password: { "S": password } } } }] } });
+        await client.send(commandWrite);
+        res.sendFile('./views/signup_landing_page.html', { root: __dirname });
+    }
+
 })
 
 
